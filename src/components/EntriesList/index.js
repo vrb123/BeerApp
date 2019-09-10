8c8cee1d8ec627c7d9acdae017ddfc900f1ed32b
vitalii <vitalii.vorobij@gmail.com>
2019-09-10 17:29:17 +0300

    added keys

--- src/components/EntriesList/index.js
+++ src/components/EntriesList/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, FlatList, StyleSheet} from 'react-native';
+import {View,Text, FlatList, StyleSheet} from 'react-native';
 
 import Entry from '../Entry';
 
